0a0aa192d2a0959f5b15fa591d767802faa5b9aa
Nathan Whitehead <nwhitehe@gmail.com>
2022-09-19 13:47:46 -0600

    Fix build

--- tools/babel.py
+++ tools/babel.py
@@ -1,5 +1,12 @@
 
+import argparse
 import subprocess
 import sys
 
-subprocess.run(['npx', 'babel', sys.argv[1], '--out-dir', sys.argv[2]])
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config')
+    parser.add_argument('--inpath')
+    parser.add_argument('--outpath')
+    args = parser.parse_args()
+    subprocess.run(['npx', 'babel', '--config-file', args.config, args.inpath, '--out-dir', args.outpath])
