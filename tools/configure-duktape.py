b6144fd97d16a96d4a8656c4d11c6a2f7bdfb1b1
Nathan Whitehead <nwhitehe@gmail.com>
2022-09-20 11:28:36 -0600

    Allow some perf flags to be set in duktape configure.

--- tools/configure-duktape.py
+++ tools/configure-duktape.py
@@ -7,5 +7,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--duktape')
     parser.add_argument('--outpath')
+    parser.add_argument('--option', action='append')
     args = parser.parse_args()
-    subprocess.run(['python2', args.duktape + '/tools/configure.py', '--output-directory', args.outpath])
+    cmdargs = ['python2', args.duktape + '/tools/configure.py', '--output-directory', args.outpath] + args.option
+    print(' '.join(cmdargs))
+    subprocess.run(cmdargs)
