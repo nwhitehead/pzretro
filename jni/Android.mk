b71dc04f2f95f6e24f03717b64eda28a865fa7a7
Rob Loach <robloach@gmail.com>
2022-09-28 02:29:28 -0400

    Add the libretro GitLab CI file

---
+++ jni/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+
+CORE_DIR := $(LOCAL_PATH)/..
+
+HAVE_NTSC := 1
+
+include $(LOCAL_PATH)/../Makefile.common
+
+COREFLAGS := $(COREDEFINES) -DPSS_STYLE=1 $(INCFLAGS)
+
+GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
+ifneq ($(GIT_VERSION)," unknown")
+	COREFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := retro
+LOCAL_SRC_FILES := $(SOURCES_C) $(SOURCES_CXX)
+LOCAL_CFLAGS    := $(COREFLAGS)
+LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/link.T
+include $(BUILD_SHARED_LIBRARY)
